1bdbe1b9e616c64634285ddc067e4432d07756d0
Jarod <jarodsim@gmail.com>
2020-12-20 17:03:37 -0300

    feat: adicionando menu responsivo

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 import { motion } from 'framer-motion';
 
 export default function Home() {
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Jarod Mateus</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta charset="utf-8" />
+        <meta name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="Site sobre Jarod Mateus" />
+        <meta name="author" content="Jarod Mateus" />
       </Head>
       <motion.header className={styles.header} initial="hidden" animate="visible" variants={{
         hidden: {
@@ -25,13 +33,29 @@ export default function Home() {
       }}>
         <h1>JAROD MATEUS</h1>
 
-        <ul>
+        <ul className={styles.menu_desktop}>
           <li><a href="/">About</a></li>
           <li><a href="/">Technologies</a></li>
           <li><a href="/">Projects</a></li>
           <li><a href="/">Talk to me</a></li>
         </ul>
+
+        <div className={styles.menu_mobile}>
+          <button className={styles.button_open_menu} onClick={() => setShowMenu(!showMenu)}><img src="/menu.png" alt="open menu icon" /></button>
+        </div>
       </motion.header>
+
+      {showMenu ? (
+        <div className={styles.content_menu_mobile}>
+          <button className={styles.button_close_menu} onClick={() => setShowMenu(!showMenu)}><img src="/close.png" alt="close menu icon" /></button>
+          <ul className={styles.menu_mobile_list}>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Technologies</a></li>
+            <li><a href="/">Projects</a></li>
+            <li><a href="/">Talk to me</a></li>
+          </ul>
+        </div>
+      ) : ''}
 
       <motion.div className={styles.content} initial="hidden" animate="visible" variants={{
         hidden: {
@@ -46,11 +70,13 @@ export default function Home() {
           }
         },
       }}>
-        <h1>Hello, my name is <span>Jarod Mateus</span> and I'm a fullstack web developer</h1>
-        <div className={styles.divScroll}>
-        <div className={styles.vertical}></div>
-        <p>SCROLL DOWN</p>
-        <small>Site under development</small>
+        <div className={styles.title}>
+          <h1>Hello, my name is <span>Jarod Mateus</span> and I'm a fullstack web developer</h1>
+        </div>
+        <div className={styles.div_scroll}>
+          <div className={styles.vertical}></div>
+          <p>SCROLL DOWN</p>
+          <small>Site under development</small>
         </div>
       </motion.div>
     </div>
