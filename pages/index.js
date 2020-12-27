28a90f71ea4f7a90639746ccfc0f729eec657413
Jarod <jarodsim@gmail.com>
2020-12-27 18:29:41 -0300

    feat: adicionando analictycs

--- pages/index.js
+++ pages/index.js
@@ -2,113 +2,180 @@ import { useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion'
 
 import About from './about'
 
 export default function Home() {
-  const [showMenu, setShowMenu] = useState(false)
+	const [showMenu, setShowMenu] = useState(false)
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Jarod Mateus</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta charSet="utf-8" />
-        <meta name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="Site sobre Jarod Mateus" />
-        <meta name="author" content="Jarod Mateus" />
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Jarod Mateus</title>
+				<link rel="icon" href="/favicon.ico" />
+				<meta charSet="utf-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, shrink-to-fit=no"
+				/>
+				<meta name="description" content="Site sobre Jarod Mateus" />
+				<meta name="author" content="Jarod Mateus" />
 
-        {/* local fonts */}
-        <link
-            rel="preload"
-            href="/fonts/Glacial/GlacialIndifference-Regular.otf"
-            as="font"
-            crossOrigin=""
-          />
-      </Head>
-      {/* header */}
-      <motion.header className={styles.header} initial="hidden" animate="visible" variants={{
-        hidden: {
-          scale: .8,
-          opacity: 0
-        },
-        visible: {
-          scale: 1,
-          opacity: 1,
-          transition: {
-            delay: .5
-          }
-        },
-      }}>
-        <div className={styles.logo}>
-          <h1>JAROD MATEUS</h1>
-        </div>
+				{/* Global site tag (gtag.js) - Google Analytics */}
+				<script
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-HRD1LD70WQ"
+				></script>
+				<script>
+					window.dataLayer = window.dataLayer || [] function gtag()
+					{dataLayer.push(arguments)}
+					gtag('js', new Date()) gtag('config', 'G-HRD1LD70WQ')
+				</script>
 
-        <ul className={styles.menu_desktop}>
-          <li><a href="#about">About</a></li>
-          <li><a href="/">Technologies</a></li>
-          <li><a href="/">Projects</a></li>
-          <li><a href="/">Talk to me</a></li>
-        </ul>
+				{/* local fonts */}
+				<link
+					rel="preload"
+					href="/fonts/Glacial/GlacialIndifference-Regular.otf"
+					as="font"
+					crossOrigin=""
+				/>
+			</Head>
+			{/* header */}
+			<motion.header
+				className={styles.header}
+				initial="hidden"
+				animate="visible"
+				variants={{
+					hidden: {
+						scale: 0.8,
+						opacity: 0,
+					},
+					visible: {
+						scale: 1,
+						opacity: 1,
+						transition: {
+							delay: 0.5,
+						},
+					},
+				}}
+			>
+				<div className={styles.logo}>
+					<h1>JAROD MATEUS</h1>
+				</div>
 
-        <div className={styles.menu_mobile}>
-          <button className={styles.button_open_menu} onClick={() => setShowMenu(!showMenu)}><img src="/icons/menu.png" alt="open menu icon" /></button>
-        </div>
-      </motion.header>
+				<ul className={styles.menu_desktop}>
+					<li>
+						<a href="#about">About</a>
+					</li>
+					<li>
+						<a href="/">Technologies</a>
+					</li>
+					<li>
+						<a href="/">Projects</a>
+					</li>
+					<li>
+						<a href="/">Talk to me</a>
+					</li>
+				</ul>
 
-      {/* menu responsivo - mobile */}
-      {showMenu ? (
-        <motion.div className={styles.content_menu_mobile} initial="hidden" animate="visible" variants={{
-          hidden: {
-            scale: .8,
-            opacity: 0
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: .2
-            }
-          },
-        }}>
-          <button className={styles.button_close_menu} onClick={() => setShowMenu(!showMenu)}><img src="/icons/close.png" alt="close menu icon" /></button>
-          <ul className={styles.menu_mobile_list}>
-            <li><a href="#about" onClick={() => setShowMenu(false)}>About</a></li>
-            <li><a href="/" onClick={() => setShowMenu(false)}>Technologies</a></li>
-            <li><a href="/" onClick={() => setShowMenu(false)}>Projects</a></li>
-            <li><a href="/" onClick={() => setShowMenu(false)}>Talk to me</a></li>
-          </ul>
-        </motion.div>
-      ) : ''}
+				<div className={styles.menu_mobile}>
+					<button
+						className={styles.button_open_menu}
+						onClick={() => setShowMenu(!showMenu)}
+					>
+						<img src="/icons/menu.png" alt="open menu icon" />
+					</button>
+				</div>
+			</motion.header>
 
-      {/* content - main */}
-      <motion.div className={styles.content} initial="hidden" animate="visible" variants={{
-        hidden: {
-          scale: .8,
-          opacity: 0
-        },
-        visible: {
-          scale: 1,
-          opacity: 1,
-          transition: {
-            delay: .5
-          }
-        },
-      }}>
-        <div className={styles.title}>
-          <h1>Hello, my name is <span>Jarod Mateus</span> and I'm a fullstack web developer</h1>
-        </div>
+			{/* menu responsivo - mobile */}
+			{showMenu ? (
+				<motion.div
+					className={styles.content_menu_mobile}
+					initial="hidden"
+					animate="visible"
+					variants={{
+						hidden: {
+							scale: 0.8,
+							opacity: 0,
+						},
+						visible: {
+							scale: 1,
+							opacity: 1,
+							transition: {
+								delay: 0.2,
+							},
+						},
+					}}
+				>
+					<button
+						className={styles.button_close_menu}
+						onClick={() => setShowMenu(!showMenu)}
+					>
+						<img src="/icons/close.png" alt="close menu icon" />
+					</button>
+					<ul className={styles.menu_mobile_list}>
+						<li>
+							<a href="#about" onClick={() => setShowMenu(false)}>
+								About
+							</a>
+						</li>
+						<li>
+							<a href="/" onClick={() => setShowMenu(false)}>
+								Technologies
+							</a>
+						</li>
+						<li>
+							<a href="/" onClick={() => setShowMenu(false)}>
+								Projects
+							</a>
+						</li>
+						<li>
+							<a href="/" onClick={() => setShowMenu(false)}>
+								Talk to me
+							</a>
+						</li>
+					</ul>
+				</motion.div>
+			) : (
+				''
+			)}
 
-        <div className={styles.div_scroll}>
-          <div className={styles.vertical}></div>
-          <p>SCROLL DOWN</p>
-          <small>Site under development</small>
-        </div>
-      </motion.div>
+			{/* content - main */}
+			<motion.div
+				className={styles.content}
+				initial="hidden"
+				animate="visible"
+				variants={{
+					hidden: {
+						scale: 0.8,
+						opacity: 0,
+					},
+					visible: {
+						scale: 1,
+						opacity: 1,
+						transition: {
+							delay: 0.5,
+						},
+					},
+				}}
+			>
+				<div className={styles.title}>
+					<h1>
+						Hello, my name is <span>Jarod Mateus</span> and I'm a fullstack web
+						developer
+					</h1>
+				</div>
 
-      <About/>
-    </div>
-  )
+				<div className={styles.div_scroll}>
+					<div className={styles.vertical}></div>
+					<p>SCROLL DOWN</p>
+					<small>Site under development</small>
+				</div>
+			</motion.div>
+
+			<About />
+		</div>
+	)
 }
