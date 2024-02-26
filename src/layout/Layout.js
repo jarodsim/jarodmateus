57bdb1560f29534e1dfebd81846bfebfff4e7dad
Jarod Cavalcante <jarodsim@gmail.com>
2024-02-26 15:07:24 -0300

    feat: adicionando <Analytics /> da versel

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 import { Container } from './LayoutStyles'
+import { Analytics } from "@vercel/analytics/react"
 
 export const Layout = ({ children }) => {
   return (
     <Container>
+      <Analytics />
       <Header />
       <main>{children}</main>
       <Footer />
